241ec34ecb00abf03ad245c7b48c4e837d52ad3f
Matheus <matheusfilho9@hotmail.com>
2025-12-19 16:02:06 -0300

    Revert "style: login sem scroll e fundo cor solida"
    This reverts commit f17a0c0dd1cde427d44a72887046fc766ab9f890.

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -65,7 +65,7 @@ const Login = () => {
   };
 
   return (
-    <div className="h-screen w-full flex items-center justify-center p-4 overflow-hidden">
+    <div className="min-h-screen w-full flex items-center justify-center p-4">
       <motion.div
         initial={{ opacity: 0, y: -50 }}
         animate={{ opacity: 1, y: 0 }}
